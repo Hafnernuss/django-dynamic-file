abf802f431ab5d885b46aab8a0fccfd12151273d
Philipp Hafner <phi.hafner@gmail.com>
2022-11-16 20:15:02 +0100

    added vscode settings and first test method

--- dynamic_file/tests/test_model_file.py
+++ dynamic_file/tests/test_model_file.py
@@ -1,9 +1,12 @@
 from django.test import TestCase
 
+from dynamic_file.models import DynamicFile
 
 
 class DynamicFileModelTestCase(TestCase):
-    
-    def test_create(self):
 
-        assert True is True
+    def test_create_default(self):
+        model = DynamicFile.objects.create()
+
+        assert model.description == ''
+        assert model.uploaded_by is None
