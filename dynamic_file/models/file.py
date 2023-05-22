da043d030f5f74f04f4039de3fb20d364ca609ff
Philipp Hafner <phi.hafner@gmail.com>
2023-05-22 18:21:58 +0200

    use correct storage

--- dynamic_file/models/file.py
+++ dynamic_file/models/file.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.utils.translation import gettext as _
 from dynamic_file.models.base import DynamicFileBase
+from dynamic_file.storage import DynamicFileSystemStorage
+
+fs = DynamicFileSystemStorage()
 
 
 class DynamicFile(DynamicFileBase):
     file = models.FileField(
+        storage=fs,
         help_text=_('The uploaded file')
     )
     '''
